5977089d78893902fcaa77dcf235c1edfe806e31
Woody Lam <cwl46@cam.ac.uk>
2021-09-01 00:18:38 +0100

    Adding dynamics calculations in simulator

--- lander.cpp
+++ lander.cpp
@@ -13,28 +13,70 @@
 // ahg@eng.cam.ac.uk and gc121@eng.cam.ac.uk.
 
 #include "lander.h"
+#include "math.h"
 
-void autopilot (void)
-  // Autopilot to adjust the engine throttle, parachute and attitude control
+void autopilot(void)
+// Autopilot to adjust the engine throttle, parachute and attitude control
 {
   // INSERT YOUR CODE HERE
 }
 
-void numerical_dynamics (void)
-  // This is the function that performs the numerical integration to update the
-  // lander's pose. The time step is delta_t (global variable).
+void numerical_dynamics(void)
+// This is the function that performs the numerical integration to update the
+// lander's pose. The time step is delta_t (global variable).
 {
-  // INSERT YOUR CODE HERE
+  static vector3d previous_position;
+  double fuel_mass = fuel * FUEL_CAPACITY * FUEL_DENSITY;
+  double combined_mass = UNLOADED_LANDER_MASS + fuel_mass;
+
+  vector3d gravity = -GRAVITY * MARS_MASS * combined_mass / position.abs2() * position.norm();
+  vector3d drag = (parachute_status == DEPLOYED) ? drag_with_parachute(position) : drag_without_parachute(position);
+
+  vector3d net_force = gravity + drag + thrust_wrt_world();
+  vector3d net_acceleration = net_force / combined_mass;
+
+  vector3d new_position = (simulation_time == 0.0) ? euler_update(net_acceleration) : verlet_update(net_acceleration, previous_position);
+
+  previous_position = position;
+  position = new_position;
 
   // Here we can apply an autopilot to adjust the thrust, parachute and attitude
-  if (autopilot_enabled) autopilot();
+  if (autopilot_enabled)
+    autopilot();
 
   // Here we can apply 3-axis stabilization to ensure the base is always pointing downwards
-  if (stabilized_attitude) attitude_stabilization();
+  if (stabilized_attitude)
+    attitude_stabilization();
 }
 
-void initialize_simulation (void)
-  // Lander pose initialization - selects one of 10 possible scenarios
+vector3d drag_without_parachute(vector3d position)
+{
+  double lander_area = M_PI * LANDER_SIZE * LANDER_SIZE;
+  return -0.5 * atmospheric_density(position) * DRAG_COEF_LANDER * lander_area * velocity.abs2() * velocity.norm();
+}
+
+vector3d drag_with_parachute(vector3d position)
+{
+  double parachute_area = 5.0 * 2.0 * LANDER_SIZE * 2.0 * LANDER_SIZE;
+  return -0.5 * atmospheric_density(position) * DRAG_COEF_CHUTE * parachute_area * velocity.abs2() * velocity.norm();
+}
+
+vector3d euler_update(vector3d net_acceleration)
+{
+  vector3d new_position = position + delta_t * velocity;
+  velocity = velocity + delta_t * net_acceleration;
+  return new_position;
+}
+
+vector3d verlet_update(vector3d net_acceleration, vector3d previous_position)
+{
+  vector3d new_position = 2 * position - previous_position + delta_t * delta_t * net_acceleration;
+  velocity = (new_position - position) / delta_t;
+  return new_position;
+}
+
+void initialize_simulation(void)
+// Lander pose initialization - selects one of 10 possible scenarios
 {
   // The parameters to set are:
   // position - in Cartesian planetary coordinate system (m)
@@ -55,11 +97,12 @@ void initialize_simulation (void)
   scenario_description[8] = "";
   scenario_description[9] = "";
 
-  switch (scenario) {
+  switch (scenario)
+  {
 
   case 0:
     // a circular equatorial orbit
-    position = vector3d(1.2*MARS_RADIUS, 0.0, 0.0);
+    position = vector3d(1.2 * MARS_RADIUS, 0.0, 0.0);
     velocity = vector3d(0.0, -3247.087385863725, 0.0);
     orientation = vector3d(0.0, 90.0, 0.0);
     delta_t = 0.1;
@@ -81,7 +124,7 @@ void initialize_simulation (void)
 
   case 2:
     // an elliptical polar orbit
-    position = vector3d(0.0, 0.0, 1.2*MARS_RADIUS);
+    position = vector3d(0.0, 0.0, 1.2 * MARS_RADIUS);
     velocity = vector3d(3500.0, 0.0, 0.0);
     orientation = vector3d(0.0, 0.0, 90.0);
     delta_t = 0.1;
@@ -92,7 +135,7 @@ void initialize_simulation (void)
 
   case 3:
     // polar surface launch at escape velocity (but drag prevents escape)
-    position = vector3d(0.0, 0.0, MARS_RADIUS + LANDER_SIZE/2.0);
+    position = vector3d(0.0, 0.0, MARS_RADIUS + LANDER_SIZE / 2.0);
     velocity = vector3d(0.0, 0.0, 5027.0);
     orientation = vector3d(0.0, 0.0, 0.0);
     delta_t = 0.1;
@@ -134,6 +177,5 @@ void initialize_simulation (void)
 
   case 9:
     break;
-
   }
 }
